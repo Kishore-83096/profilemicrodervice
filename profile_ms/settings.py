697849feaab1ba9c723edbccdab1d3e8118136ed
Kishore Siripurapu <kishore.siripurapu1484@gmail.com>
2026-01-21 16:37:50 +0530

    fixed connection issues 3

--- profile_ms/settings.py
+++ profile_ms/settings.py
@@ -150,12 +150,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-CORS_ALLOWED_ORIGINS = [
-    origin for origin in os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
-    if origin
-]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    origin for origin in os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
-    if origin
+    "https://authmicroservise.onrender.com",
+    "https://profilemicrodervice.onrender.com",
+    "http://localhost:5173",
+    "http://localhost:3000",
 ]
